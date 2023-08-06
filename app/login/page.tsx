1445af7f77f15a5daff221aa77e38ea4f085f2ae
kazuwombat <matsumotokazuya7@gmail.com>
2023-08-06 17:46:53 +0900

    init

---
+++ app/login/page.tsx
@@ -0,0 +1,25 @@
+/** @format */
+
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import AuthButtonClient from "../auth-button-client"
+
+export default async function Login() {
+  const supabase = createServerComponentClient({ cookies })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (session) {
+    redirect("/")
+  }
+
+  return (
+    <div>
+      <div>ログインページ</div>
+      <AuthButtonClient session={session} />
+    </div>
+  )
+}
